ed0d110f270358eb4e88d6fd6ce08c96815af9bf
suann-c <c.sueey@gmail.com>
2022-04-26 00:23:43 -0400

    generating a white gif. still debugging

--- convertToGif.py
+++ convertToGif.py
@@ -3,10 +3,19 @@ import os
 import argparse
 import numpy as np
 import torchvision.utils as vutils
+import torchvision.transforms as transforms
+from PIL import Image
 
 #Code to convert images to gif from CMU 16-726 HW5
 def save_images(image, fname, col=8):
-    image = image.cpu().detach()
+    #image = image.cpu().detach()
+    #open image here
+    transform = transforms.Compose([
+        transforms.PILToTensor()
+    ])
+    image = Image.open(image)
+    image = transform(image)
+
     image = image / 2 + 0.5
 
     image = vutils.make_grid(image, nrow=col)  # (C, H, W)
@@ -32,19 +41,23 @@ def parse_arg():
     """Creates a parser for command-line arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inputDir', type=str, default='input/*.png', help="path to the input image")
-
+    parser.add_argument('--inputDir', type=str, default='~/Desktop/depth', help="path to the input image")
+    parser.add_argument("--envName", type=str, default="2n8kARJN3HM")
+    parser.add_argument("--trajectoryName", type=str, default="110")
+    parser.add_argument("--gifType", type=str, default="depth")
     return parser.parse_args()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = parse_arg()
-    print(args)
-
     #save gif of depth, rgb, and semantic information
-    image_list = args.inputDir
-    for i in range(len(image_list)):
-        outputDir = 'output/%d_%s' % (i, image_list[i])
-        save_gifs(image_list, outputDir)
+    imagesPath = os.path.expanduser(args.inputDir)+"/"
+    imagePathsList = os.listdir(imagesPath)
+    imagePathsList.sort() #organize these by correct order
+    image_list = [imagesPath+s for s in imagePathsList]
+    print(image_list)
+    outputDir = '%s/output/%s_%s_%s' % (os.path.expanduser("~/Desktop"), args.envName, args.trajectoryName, args.gifType)
+    print(outputDir)
+    save_gifs(image_list, outputDir)
 
