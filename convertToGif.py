605371e7f81403cbf99d15398f7e22431c3188ed
suann-c <c.sueey@gmail.com>
2022-04-26 12:53:33 -0400

    fixed convert to gif

--- convertToGif.py
+++ convertToGif.py
@@ -5,7 +5,8 @@ import numpy as np
 import torchvision.utils as vutils
 import torchvision.transforms as transforms
 from PIL import Image
-import tensorflow as tf
+import re
+#import tensorflow as tf
 
 #Code to convert images to gif from CMU 16-726 HW5
 def save_images(image, fname, gifType, col=8):
@@ -16,8 +17,8 @@ def save_images(image, fname, gifType, col=8):
     ])
     image = transform(Image.open(image))
 
-    if (gifType=="rgb"): #if rgb then flip the channels
-        tf.reverse(image, axis=0)
+    # if (gifType=="rgb"): #if rgb then flip the channels
+    #     tf.reverse(image, axis=0)
 
     image = vutils.make_grid(image, nrow=col)  # (C, H, W)
     image = image.numpy().transpose([1, 2, 0])
@@ -42,12 +43,17 @@ def parse_arg():
     """Creates a parser for command-line arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--inputDir', type=str, default='~/Desktop/semantics', help="path to the input image")
+    parser.add_argument('--inputDir', type=str, default='~/Desktop/rgb', help="path to the input image")
     parser.add_argument("--envName", type=str, default="2n8kARJN3HM")
     parser.add_argument("--trajectoryName", type=str, default="110")
-    parser.add_argument("--gifType", type=str, default="semantics")
+    parser.add_argument("--gifType", type=str, default="rgb")
     return parser.parse_args()
 
+#logic from https://www.tutorialspoint.com/How-to-correctly-sort-a-string-with-a-number-inside-in-Python
+def atoi(text):
+    return int(text) if text.isdigit() else text
+def naturalKeys(text):
+    return [ atoi(c) for c in re.split('(\d+)',text) ]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -55,7 +61,8 @@ if __name__ == '__main__':
     #save gif of depth, rgb, and semantic information
     imagesPath = os.path.expanduser(args.inputDir)+"/"
     imagePathsList = os.listdir(imagesPath)
-    imagePathsList.sort() #organize these by correct order
+    imagePathsList.sort(key=naturalKeys)
+    print(imagePathsList)
     #TODO: FIGURE OUT A WAY TO IGNORE NON .PNG FILES
     image_list = [imagesPath+s for s in imagePathsList]
     print(image_list)
