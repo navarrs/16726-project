f232987c36cab0705e98cea65ed88aeed8640adb
suann-c <c.sueey@gmail.com>
2022-04-25 23:33:03 -0400

    statistics py before testing

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 
 """
@@ -8,12 +9,39 @@ minimum, maximum and average trajectory lengths, and the number of environments 
 
 def calculateStatistics(inputDir):
     totalTrajectories = 0
-    minTrajectoryLen = 0
-    maxTrajectoryLen = 0
-    avgTrajectoryLen = 0
+    trajectoryLens = []
     numEnvs = 0
+
+    #iteration code from: https://www.geeksforgeeks.org/how-to-iterate-over-files-in-directory-using-python/
+    for env in os.listdir(inputDir):
+        numEnvs += 1 #first layer of directory is the number of environments
+        fEnv = os.path.join(inputDir, env)
+        if os.path.isfile(fEnv): # checking if it is a file. should not be file yet
+            raise Exception("There should be additional trajectories within env folder.")
+        envPath = inputDir+fEnv
+        print(envPath)
+
+        for traj in os.listdir(envPath): #iterate through all trajectories in each env subfolder
+            totalTrajectories += 1 #second layer of directory is the number of trajectories
+            fTraj = os.path.join(envPath, traj)
+            if os.path.isfile(fTraj):  # checking if it is a file. should not be file yet
+                raise Exception("There should be depth, semantic, and rgb info within trajectory folder.")
+            #take the first folder in this case to check len.from
+            depthDir = os.path.join(inputDir, traj)
+            if os.path.isfile(depthDir): #should also not be a file.
+                raise Exception("There should be pictures in the depthDir.")
+
+            tmpTrajectoryLen = 0
+            for img in os.listdir(depthDir): #collect trajectory len info.
+                tmpTrajectoryLen += 1
+            trajectoryLens.append(tmpTrajectoryLen)
+
+    minTrajectoryLen = min(trajectoryLens)
+    maxTrajectoryLen = max(trajectoryLens)
+    avgTrajectoryLen = sum(trajectoryLens)/len(trajectoryLens)
     return totalTrajectories, minTrajectoryLen, maxTrajectoryLen, avgTrajectoryLen, numEnvs
 
+#Code for parse_arg and main from CMU 16-726 HW5
 def parse_arg():
     """Creates a parser for command-line arguments.
     """
